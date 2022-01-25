cd737b4bd504938a7e0709670a4cc58f9c1a4c5a
Elistratishka <elistratishka@outlook.com>
2022-01-26 00:23:50 +0300

    1

---
+++ News_Portal/News/templatetags/custom_filters.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='censor')
+def censor(value):
+    bad_words = ['kaka', 'saka', 'nigga']
+    text = set(value.split())
+    for i in text:
+        for j in bad_words:
+            if i == j:
+                return value.replace(i, '***')
+    return value
